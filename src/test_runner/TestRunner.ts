8507b899ca7dfebd10198f9101612371313788e0
nicojs <jansennico@gmail.com>
2016-06-09 07:39:57 +0200

    fix(TestRunner) Replace TestRunner base class with interface

--- src/test_runner/TestRunner.ts
+++ src/test_runner/TestRunner.ts
@@ -6,20 +6,14 @@ import RunOptions from './RunOptions';
 /**
  * Represents a TestRunner which can execute tests, resulting in a RunResult.
  */
-abstract class TestRunner {
-  /**
-   * Base constructor for the TestRunner.
-   * @param options The configuration options for the TestRunner.
-   */
-  constructor(protected options: RunnerOptions) {
-  }
+interface TestRunner {
 
   /**
    * Executes a test run.
    * @param options The options for this test run.
    * @returns A promise to eventually complete the test run and deliver a RunResult.
    */
-  abstract run(options: RunOptions): Promise<RunResult>;
+  run(options: RunOptions): Promise<RunResult>;
 }
 
 export default TestRunner;
