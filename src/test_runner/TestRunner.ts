94e61c7d625bdc85d427b284631903bfce6b7b27
Nico Jansen <jansennico@gmail.com>
2016-07-14 08:16:47 +0200

    feat(testRunner): Add lifecycle events. (#1)
    Add an init and dispose method to the TestRunner interface, to give implementers the change to do some (async) initialisation and cleaning before and after all test runs.

--- src/test_runner/TestRunner.ts
+++ src/test_runner/TestRunner.ts
@@ -9,11 +9,25 @@ import RunOptions from './RunOptions';
 interface TestRunner {
 
   /**
+   * Optional. When implemented, will be called before runs are done on this test runner.
+   * @returns A promise if stuff is initialized asyncronously, runs will not start until the promise is resolved.
+   * Otherwise void
+   */
+  init?(): Promise<any> | void;
+
+  /**
    * Executes a test run.
    * @param options The options for this test run.
    * @returns A promise to eventually complete the test run and deliver a RunResult.
    */
   run(options: RunOptions): Promise<RunResult>;
+
+  /**
+   * Optional. When implemented, will be called before the test runner's process is killed.
+   * @returns A promise if stuff is destroyed asyncronously, the runners process will not end until the promise is resolved.
+   * Otherwise void 
+   */
+  dispose?(): Promise<any> | void;
 }
 
 export default TestRunner;
