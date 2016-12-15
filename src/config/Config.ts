ee46042054ff21ef3a3764b7484429b3b4dd4fb7
Nico Jansen <jansennico@gmail.com>
2016-12-15 08:13:31 +0100

    fix(config): Update `files` array type
    * Set `files` array type to correct one (`Array<string | InputFileDescriptor>`)

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,19 +1,18 @@
-
-import {StrykerOptions} from '../../core';
+import { StrykerOptions, InputFileDescriptor } from '../../core';
 
 export default class Config implements StrykerOptions {
 
   [customConfig: string]: any;
 
-  files: string[];
+  files: Array<string | InputFileDescriptor>;
   mutate: string[];
-  
+
   logLevel = 'info';
   timeoutMs = 5000;
   timeoutFactor = 1.5;
   plugins: string[] = ['stryker-*'];
   port = 9234;
-  reporter = ['progress', 'clear-text']; 
+  reporter = ['progress', 'clear-text'];
   coverageAnalysis: 'perTest' | 'all' | 'off' = 'perTest';
   testRunner: string;
   testFramework: string;
