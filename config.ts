97794ed33169fe808624d163dc2e37620d59d4cd
Simon de Lang <simondelang@gmail.com>
2017-04-07 13:36:44 +0200

    chore(stryker-api): Delete stryker-api

--- config.ts
+++
@@ -1,3 +0,0 @@
-export {default as Config} from './src/config/Config';
-export {default as ConfigWriter} from './src/config/ConfigWriter';
-export {default as ConfigWriterFactory} from './src/config/ConfigWriterFactory';
--- mutant.ts
+++
@@ -1,2 +0,0 @@
-export {default as Mutator} from './src/mutant/Mutator';
-export {default as MutatorFactory} from './src/mutant/MutatorFactory';
