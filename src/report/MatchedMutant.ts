43336688b530e93a30505c2bb6fe1d0a8a91cae2
Simon de Lang <simondelang@gmail.com>
2017-04-10 07:59:30 +0200

    refactro(stryker-api): Move to sub package of main stryker repo (#19)
    * docs(README): Redirect to Stryker
    * refactor(stryker-api): Delete stryker-api

--- src/report/MatchedMutant.ts
+++
@@ -1,9 +0,0 @@
-interface MatchedMutant {
-  readonly mutatorName: string;
-  readonly scopedTestIds: number[];
-  readonly timeSpentScopedTests: number;
-  readonly filename: string;
-  readonly replacement: string;
-}
-
-export default MatchedMutant;
